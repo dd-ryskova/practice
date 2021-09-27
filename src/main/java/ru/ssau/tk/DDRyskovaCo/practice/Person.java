c760269059073cf4bc701e9befc954c0756ebb1b
Ryskova <ddryskova2002@gmail.com>
2021-09-28 00:59:27 +0400

    Задание 1.2

--- src/main/java/ru/ssau/tk/DDRyskovaCo/practice/Person.java
+++ src/main/java/ru/ssau/tk/DDRyskovaCo/practice/Person.java
@@ -2,6 +2,11 @@ package ru.ssau.tk.DDRyskovaCo.practice;
 
 public class Person {
 
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.lastName + " Passport: " + passportId + " ";
+    }
+
     private String firstName;
     private String lastName;
     private int passportId;
@@ -30,19 +35,48 @@ public class Person {
         this.passportId = passportId;
     }
 
+    public Person() {
+        this.firstName = "";
+        this.lastName = "";
+        this.passportId = 0;
+        System.out.println("  First constructor");
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passportId = 0;
+        System.out.println("  Second constructor");
+    }
+
+    public Person(int passportId) {
+        this.firstName = "";
+        this.lastName = "";
+        this.passportId = passportId;
+        System.out.println("  Third constructor");
+    }
+
+    public Person(String firstName, String lastName, int passportId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passportId = passportId;
+        System.out.println("  Fourth constructor");
+    }
+
+
     public static void main(String[] args) {
-        Person first = new Person();
-        first.setFirstName("Jane");
-        first.setLastName("Austen");
-        first.setPassportId(234567);
+        Person first = new Person("Jane", "Austen", 234567);
+        Person second = new Person("Stephen", "King");
+        Person third = new Person(349812);
+        Person fourth = new Person("Dale", "Carnegie", 123457);
+        Person fifth = new Person();
 
-        Person second = new Person();
-        second.setFirstName("Stephen");
-        second.setLastName("King");
-        second.setPassportId(456787);
-
-        System.out.println("First human: " + first.getFirstName() + " " + first.getLastName() + "   " + "Passport: " + first.getPassportId());
-        System.out.println("Second human: " + second.getFirstName() + " " + second.getLastName() + "   " + "Passport: " + second.getPassportId());
+        System.out.println();
+        System.out.println("First human: " + first);
+        System.out.println("Second human: " + second);
+        System.out.println("Third human: " + third);
+        System.out.println("Fourth human: " + fourth);
+        System.out.println("Fifth human: " + fifth);
     }
 }
 
