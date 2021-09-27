4c07cf2833d685a3da42da14c85ed1395882e604
Ryskova <ddryskova2002@gmail.com>
2021-09-28 00:05:18 +0400

    Создание класса Person

---
+++ src/main/java/ru/ssau/tk/DDRyskovaCo/practice/Person.java
@@ -0,0 +1,38 @@
+package ru.ssau.tk.DDRyskovaCo.practice;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int passportId;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getPassportId() {
+        return passportId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPassportId(int passportId) {
+        this.passportId = passportId;
+    }
+
+}
+
+
+
+
+
