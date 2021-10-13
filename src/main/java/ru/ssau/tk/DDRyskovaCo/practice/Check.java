d334badcb8b13021b258598c221972e7c3fdec6a
dasha <ddryskova2002@gmail.ru>
2021-10-14 00:26:20 +0400

    Задание 1.14

--- src/main/java/ru/ssau/tk/DDRyskovaCo/practice/Check.java
+++ src/main/java/ru/ssau/tk/DDRyskovaCo/practice/Check.java
@@ -3,21 +3,14 @@ package ru.ssau.tk.DDRyskovaCo.practice;
 public class Check {
     public static void main(String[] args) {
         Person person = new Person();
-        person.setFirstName("Arkadiy");
-        int number = 5;
-        checkInt(number);                                //Здесь мы получаем 10 из класса checkInt и выводим его
-        checkPerson(person);                             //Из класса checkPerson меняем значение person.setFirstName на Oleg и выводим
-        System.out.println(number);                      // 5 получаем из данного класса
-        System.out.println(person.getFirstName());       //Выводим Oleg, так как поле FirstName было изменено
+        person.setFirstName("Arkadiy");                       //Создаём первый объект с именем Arkadiy
+        checkAnotherPerson(person);                           //Вызываем метод checkAnotherPerson(person) и выводим имя Ignat
+        System.out.println(person.getFirstName());            //Выводим имя Arkadiy
     }
 
-    private static void checkInt(int number) {
-        number = 10;
-        System.out.println(number);
-    }
-
-    private static void checkPerson(Person person) {
-        person.setFirstName("Oleg");
+    private static void checkAnotherPerson(Person person) {
+        person = new Person();                                //Создаём второй объект с именем Ignat, даже если в качестве параметра принимаем - первый
+        person.setFirstName("Ignat");
         System.out.println(person.getFirstName());
     }
 }
