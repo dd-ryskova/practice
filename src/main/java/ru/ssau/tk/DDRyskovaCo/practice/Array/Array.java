cd091b79c297430d15a24ccbd7d2ca63ce93bf2f
dasha <ddryskova2002@gmail.ru>
2021-10-16 01:24:48 +0400

    Задание 2.2

--- src/main/java/ru/ssau/tk/DDRyskovaCo/practice/Array/Array.java
+++ src/main/java/ru/ssau/tk/DDRyskovaCo/practice/Array/Array.java
@@ -5,4 +5,14 @@ public class Array {
     public static int[] createArray(int size) {
         return new int[size];
     }
+
+    public static int[] createFilledArray(int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < size; ++i) {
+            array[i] = 1;
+            array[0] = 2;
+            array[size-1] = 2;
+        }
+        return array;
+    }
 }
--- src/test/java/ru/ssau/tk/DDRyskovaCo/practice/Array/ArrayTest.java
+++ src/test/java/ru/ssau/tk/DDRyskovaCo/practice/Array/ArrayTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 import static ru.ssau.tk.DDRyskovaCo.practice.Array.Array.createArray;
+import static ru.ssau.tk.DDRyskovaCo.practice.Array.Array.createFilledArray;
 
 public class ArrayTest {
 
@@ -11,10 +12,23 @@ public class ArrayTest {
     int[] secondArray = createArray(5);
     int[] thirdArray = createArray(0);
 
+    int[] fourthArray = createFilledArray(14);
+    int[] fifthArray = createFilledArray(5);
+
     @Test
     public void testCreateArray() {
         assertEquals(firstArray.length, 14);
         assertEquals(secondArray.length, 5);
         assertEquals(thirdArray.length, 0);
+    }
+
+    @Test
+    public void testCreateFilledArray() {
+        assertEquals(fourthArray[0], 2);
+        assertEquals(fourthArray[13], 2);
+        assertEquals(fourthArray[5], 1);
+        assertEquals(fifthArray[0], 2);
+        assertEquals(fifthArray[4], 2);
+        assertEquals(fifthArray[3], 1);
     }
 }
