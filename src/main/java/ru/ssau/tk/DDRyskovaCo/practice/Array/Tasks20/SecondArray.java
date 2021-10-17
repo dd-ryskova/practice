f5238e35a93c18e323f4a3e40f6cc42675f3d452
dasha <ddryskova2002@gmail.ru>
2021-10-18 00:34:52 +0400

    Задание 2.13

--- src/main/java/ru/ssau/tk/DDRyskovaCo/practice/Array/Tasks20/SecondArray.java
+++ src/main/java/ru/ssau/tk/DDRyskovaCo/practice/Array/Tasks20/SecondArray.java
@@ -1,5 +1,7 @@
 package ru.ssau.tk.DDRyskovaCo.practice.Array.Tasks20;
 
+import static java.lang.Math.pow;
+
 public class SecondArray {
 
     public static int[] createArrayOfDivisors(int x) {
@@ -15,6 +17,21 @@ public class SecondArray {
             if (x % i == 0) {
                 array[j++] = i;
             }
+        }
+        return array;
+    }
+
+    public static int[] createSymmetricArray(int size) {
+        int[] array = new int[size];
+        int index;
+        if (size % 2 != 0) {
+            index = (size / 2) + 1;
+        } else {
+            index = size / 2;
+        }
+        for (int i = 1; i <= index; i++) {
+            array[i - 1] = i;
+            array[size - i] = i;
         }
         return array;
     }
--- src/test/java/ru/ssau/tk/DDRyskovaCo/practice/Array/Tasks20/SecondArrayTest.java
+++ src/test/java/ru/ssau/tk/DDRyskovaCo/practice/Array/Tasks20/SecondArrayTest.java
@@ -2,12 +2,17 @@ package ru.ssau.tk.DDRyskovaCo.practice.Array.Tasks20;
 
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+
 import static org.testng.Assert.*;
 import static ru.ssau.tk.DDRyskovaCo.practice.Array.Tasks20.SecondArray.*;
+
 
 public class SecondArrayTest {
 
     public int[] firstArray = createArrayOfDivisors(45);
+    public int[] secondArray = createSymmetricArray(7);
+    private int[] thirdArray = createSymmetricArray(8);
 
     @Test
     public void testCreateArrayOfDivisors() {
@@ -15,5 +20,14 @@ public class SecondArrayTest {
         assertEquals(firstArray[0], 1);
         assertEquals(firstArray[1], 3);
         assertEquals(firstArray[2], 5);
+    }
+
+    @Test
+    public void testCreateSymmetricArray() {
+        int[] oneArray = new int[]{1, 2, 3, 4, 3, 2, 1};
+        assertTrue(Arrays.equals(secondArray, oneArray));
+
+        int[] twoArray = new int[]{1, 2, 3, 4, 4, 3, 2, 1};
+        assertTrue(Arrays.equals(thirdArray, twoArray));
     }
 }
