cdfb837c20cee4962849f20306f06f93de10f85c
dasha <ddryskova2002@gmail.ru>
2021-10-26 01:18:51 +0400

    Задание 3.23

--- src/test/java/ru/ssau/tk/DDRyskovaCo/practice/String/Tasks20/ClassForOtherTasksTest.java
+++ src/test/java/ru/ssau/tk/DDRyskovaCo/practice/String/Tasks20/ClassForOtherTasksTest.java
@@ -57,7 +57,11 @@ public class ClassForOtherTasksTest {
 
     @Test
     public void testFormTheString() {
-        System.out.println(formTheString(10));
+        assertEquals(formTheString(10), "0 1 2 3 4 5 6 7 8 9 ");
         System.out.println(formTheString(10000));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(java.nio.charset.Charset.defaultCharset());
     }
 }
